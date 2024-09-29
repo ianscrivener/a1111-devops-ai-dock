3915c88178129acbe3df61e8640d389b9c9008a6
Ian Scrivener <163229+ianscrivener@users.noreply.github.com>
2024-09-29 17:13:31 +1000

    Create a1111_adetailer.sh

---
+++ a1111_adetailer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /opt/stable-diffusion-webui/models/adetailer
+
+wget -nc https://civitai.com/api/download/models/484684 -O "pussy_v41.pt"
+wget -nc https://civitai.com/api/download/models/138918 -O "boobs_v47.pt"
+wget -nc https://civitai.com/api/download/models/149588 -O "nipples.pt"
+wget -nc https://civitai.com/api/download/models/484684 -O "pussy_v41.pt"
+wget -nc https://civitai.com/api/download/models/465360 -O "eyes-individual.pt"
+
