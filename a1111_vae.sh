b74cde594439f0d7aa257c3bf674b12e391e0b8d
Ian Scrivener <163229+ianscrivener@users.noreply.github.com>
2024-09-30 12:39:54 +1000

    Update a1111_vae.sh

--- a1111_vae.sh
+++ a1111_vae.sh
@@ -3,6 +3,6 @@
 cd /opt/stable-diffusion-webui/models/VAE
 rm *.txt
 
-wget -nc https://civitai.com/api/download/models/80869 -O vae-ft-mse-840000-ema-pruned.safetensors
+# wget -nc https://civitai.com/api/download/models/80869 -O vae-ft-mse-840000-ema-pruned.safetensors
 
 
