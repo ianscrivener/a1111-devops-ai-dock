58d0ea9c3effb5669bdd3f532fe50d8762397728
Ian Scrivener <163229+ianscrivener@users.noreply.github.com>
2024-10-04 19:55:08 +1000

    add `&` and `wait` to run multiple bash scripts in parallel

--- init.sh
+++ init.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-wget -qO- https://raw.githubusercontent.com/ianscrivener/a1111-devops-ai-dock/main/a1111_custom_settings.sh | bash
+# add `&` and `wait` to run multiple bash scripts in parallel
 
-wget -qO- https://raw.githubusercontent.com/ianscrivener/a1111-devops-ai-dock/main/a1111_adetailer.sh | bash
 
-wget -qO- https://raw.githubusercontent.com/ianscrivener/a1111-devops-ai-dock/main/a1111_esrgan.sh | bash
+wget -qO- https://raw.githubusercontent.com/ianscrivener/a1111-devops-ai-dock/main/a1111_custom_settings.sh | bash &
 
-wget -qO- https://raw.githubusercontent.com/ianscrivener/a1111-devops-ai-dock/main/a1111_lora.sh | bash
+wget -qO- https://raw.githubusercontent.com/ianscrivener/a1111-devops-ai-dock/main/a1111_adetailer.sh | bash &
 
-wget -qO- https://raw.githubusercontent.com/ianscrivener/a1111-devops-ai-dock/main/a1111_vae.sh | bash
+wget -qO- https://raw.githubusercontent.com/ianscrivener/a1111-devops-ai-dock/main/a1111_esrgan.sh | bash &
 
-# wget -qO- https://raw.githubusercontent.com/ianscrivener/a1111-devops-ai-dock/main/a1111_controlnet.sh | bash
+wget -qO- https://raw.githubusercontent.com/ianscrivener/a1111-devops-ai-dock/main/a1111_lora.sh | bash &
 
-wget -qO- https://raw.githubusercontent.com/ianscrivener/a1111-devops-ai-dock/main/a1111_embeddings.sh | bash
+wget -qO- https://raw.githubusercontent.com/ianscrivener/a1111-devops-ai-dock/main/a1111_vae.sh | bash &
 
-wget -qO- https://raw.githubusercontent.com/ianscrivener/a1111-devops-ai-dock/main/a1111_extension.sh | bash
+# wget -qO- https://raw.githubusercontent.com/ianscrivener/a1111-devops-ai-dock/main/a1111_controlnet.sh | bash &
 
-wget -qO- https://raw.githubusercontent.com/ianscrivener/a1111-devops-ai-dock/main/a1111_models.sh | bash
+wget -qO- https://raw.githubusercontent.com/ianscrivener/a1111-devops-ai-dock/main/a1111_embeddings.sh | bash &
+
+wget -qO- https://raw.githubusercontent.com/ianscrivener/a1111-devops-ai-dock/main/a1111_extension.sh | bash &
+
+wget -qO- https://raw.githubusercontent.com/ianscrivener/a1111-devops-ai-dock/main/a1111_models.sh | bash &
 
 # Add MacBook Pro remote Syncthing ID and config Stable Diffusion Output folder
 # syncthing cli config devices add --name "MacBook Pro" --device-id "$SYNCTHING_REMOTE_ID"
 # syncthing cli config folders add --name "Stable Diffusion Output" --path "cd /opt/stable-diffusion-webui/outputs" --device "$SYNCTHING_REMOTE_ID"
 
+
+wait
